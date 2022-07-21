75f98d20856c9de02ac93746f160dcd0c5ce624a
experimentallife <user@experimentallife.dev>
2022-07-21 00:47:21 -0500

    :sparkles: configure typescript and dev dependencies

---
+++ webpack/webpack.common.js
@@ -0,0 +1,64 @@
+const path = require('path')
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: {
+    main: path.resolve('./src/index.tsx'),
+  },
+  output: {
+    publicPath: '/',
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.ts', '.tsx', '.js', '.html', '.txt'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.jsonc$/,
+        use: 'raw-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './src/assets/favicon.ico',
+    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerHost: "0.0.0.0", // To make it work in the container
+    // }),
+  ],
+}
